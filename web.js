279ba6ea8941029518cfa58b21514b1425836945
Tom Wood <thomas.a.wood@gmail.com>
2013-07-10 02:47:00 +0000

    Add utf-8 option to readFileSync

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var contents = fs.readFileSync("index.html");
+    var contents = fs.readFileSync("index.html", "utf-8");
     response.send(contents);
 });
 
